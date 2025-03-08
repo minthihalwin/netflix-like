24d01098235e58bbdc56bd2c99847dcb218218fa
minthihalwin <minthihalwin255@gmail.com>
2025-03-09 01:38:38 +1300

    Feat: add MovieContext for managing favorites with local storage support

---
+++ src/contexts/MovieContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const MovieContext = createContext();
+
+export const useMoveContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+
+    if (storedFavs) {
+      setFavorites(JSON.parse(storedFavs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+
+  const removeFromFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
+
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
+
+  const value = {
+    favorites,
+    addFavorites,
+    removeFromFavorites,
+    isFavorite,
+  };
+  return (
+    <MovieContext.Provider value={value}> {children}</MovieContext.Provider>
+  );
+};
